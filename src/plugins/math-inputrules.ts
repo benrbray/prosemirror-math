ec527085707d85266a116d26445ff04c2d811132
Benjamin Bray <benrbray@gmail.com>
2021-03-30 10:06:32 +0900

    bug: revert to regex literal instead of string, which was causing input rule bug

--- src/plugins/math-inputrules.ts
+++ src/plugins/math-inputrules.ts
@@ -12,21 +12,21 @@ import { NodeSelection } from "prosemirror-state";
 // ---- Inline Input Rules ------------------------------ //
 
 // simple input rule for inline math
-export const REGEX_INLINE_MATH_DOLLARS:RegExp = new RegExp("\$(.+)\$");
+export const REGEX_INLINE_MATH_DOLLARS:RegExp = /\$(.+)\$/; //new RegExp("\$(.+)\$", "i");
 
 // negative lookbehind regex notation allows for escaped \$ delimiters
 // (requires browser supporting ECMA2018 standard -- currently only Chrome / FF)
 // (see https://javascript.info/regexp-lookahead-lookbehind)
 export const REGEX_INLINE_MATH_DOLLARS_ESCAPED:RegExp = (() => {
     // attempt to create regex with negative lookbehind
-    try      { return new RegExp("(?<!\\)\$(.+)(?<!\\)\$"); }
+    try      { return new RegExp("(?<!\\)\$(.+)(?<!\\)\$", "i"); }
     catch(e) { return REGEX_INLINE_MATH_DOLLARS;            }
 })();
 
 // ---- Block Input Rules ------------------------------- //
 
 // simple inputrule for block math
-export const REGEX_BLOCK_MATH_DOLLARS:RegExp = new RegExp("/\$\$\s+$");
+export const REGEX_BLOCK_MATH_DOLLARS:RegExp = /\$\$\s+$/; //new RegExp("\$\$\s+$", "i");
 
 ////////////////////////////////////////////////////////////
 
