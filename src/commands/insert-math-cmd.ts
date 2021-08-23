0368966ec1c85ca350cdb3274687ba5160cbe1f3
Benjamin Bray <benrbray@gmail.com>
2021-08-23 20:44:29 +0900

    rename and document initialText parameter of insertMathCmd

--- src/commands/insert-math-cmd.ts
+++ src/commands/insert-math-cmd.ts
@@ -11,15 +11,16 @@ import { EditorState, NodeSelection, Transaction } from "prosemirror-state";
  * 
  * @param mathNodeType An instance for either your math_inline or math_display
  *     NodeType.  Must belong to the same schema that your EditorState uses!
+ * @param initialText (optional) The initial source content for the math editor.
  */
-export function insertMathCmd(mathNodeType: NodeType, text = ""): Command {
+export function insertMathCmd(mathNodeType: NodeType, initialText=""): Command {
 	return function(state:EditorState, dispatch:((tr:Transaction)=>void)|undefined){
 		let { $from } = state.selection, index = $from.index();
 		if (!$from.parent.canReplaceWith(index, index, mathNodeType)) {
 			return false;
 		}
 		if (dispatch){
-			let mathNode = mathNodeType.create({}, text ? state.schema.text(text) : null);
+			let mathNode = mathNodeType.create({}, initialText ? state.schema.text(initialText) : null);
 			let tr = state.tr.replaceSelectionWith(mathNode);
 			tr = tr.setSelection(NodeSelection.create(tr.doc, $from.pos));
 			dispatch(tr);
