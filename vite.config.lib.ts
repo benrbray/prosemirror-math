57fd71f8b532dac65e211ee1b8d28e890b9aa99e
Benjamin Bray <benrbray@gmail.com>
2024-04-11 17:04:41 +0900

    separate config for library build vs site build

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,5 +1,6 @@
-// vite configuration for building the GitHub Pages site
+// vite configuration for building the npm package
 
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths'
 
@@ -7,9 +8,11 @@ import pkg from "./package.json";
 
 export default defineConfig({
 	plugins: [tsConfigPaths()],
-	base: "/prosemirror-math/",
 	build: {
-		outDir: "dist-site",
+		lib: {
+			formats: ["es"],
+			entry: resolve(__dirname, 'lib/main.ts'),
+		},
 		rollupOptions: {
 			external: [
 				...Object.keys(pkg.peerDependencies || {}),
